65a41b1a9515c9632e44a03d3b3439045db6e6c8
Pranav952 <pranavb952@gmail.com>
2026-02-21 15:53:54 +0545

    chore: initial frontend setup with (react+vite+typescript)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>Hellow world</h1>
+      <p>This is helllow world</p>
+    </div>
+  )
+}
